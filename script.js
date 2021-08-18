3c8fadcdb770930a67d723335d777a16a48af0d4
Rooney25 <87777127+Rooney25@users.noreply.github.com>
2021-08-18 09:34:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
 let searchText1 = document.getElementById('searchText1');
+let searchText2 = document.getElementById('searchText2');
 document.getElementById("submit").addEventListener('click', search);
-const url = `https://api.schooldigger.com/v1.2/schools?st=NJ&zip=${searchText1.value}&appID=6bcf25ea&appKey=8b6ea6dac0161c38433bebf496cf5960`;
 let data;
- 
 function search(event) {
     event.preventDefault();
+    const url = `https://api.schooldigger.com/v1.2/schools?st=${searchText2.value}&zip=${searchText1.value}&appID=6bcf25ea&appKey=8b6ea6dac0161c38433bebf496cf5960`;
     fetch(url)
         .then(function(response) {
-            console.log("This is the response: ", response)
             return response.json();
         })
- 
         .then(function(myJson) {
             data = myJson;
             textt.innerHTML = "";
             for (i = 0; i < myJson.schoolList.length; i++) {
-                const ranName = myJson.schoolList[i].schoolName;
-                console.log("This is ranName: ", ranName);
-                const paragraph = document.createElement("p");
-                paragraph.innerText = ranName;
-                document.getElementById("textt").appendChild(paragraph);
+                if (myJson.schoolList[i].rankHistory){
+                    const ranName = myJson.schoolList[i].schoolName;
+                    const ranRat = myJson.schoolList[i].rankHistory[0].rank;
+                    const outOf = myJson.schoolList[i].rankHistory[0].rankOf;
+                    const ranStars = myJson.schoolList[i].rankHistory[0].rankStars;
+
+                    const paragraph = document.createElement("p");
+                    const text = document.createElement("ul");
+                    const sName = document.createElement("li");
+                    const sRat = document.createElement("li");
+                    const sStars = document.createElement("li");
+
+                    sName.innerText = ranName;
+                    sRat.innerText = "Rank " + ranRat + " out of " + outOf;
+                    sStars.innerText = ranStars + " stars";
+
+                    text.appendChild(sName);
+                    text.appendChild(sRat);
+                    text.appendChild(sStars);
+                    paragraph.appendChild(text);
+                    document.getElementById("textt").appendChild(paragraph);
+                }
             }
-            
         })
- 
 }
